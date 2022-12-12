a2ed80a93140b9e21a3f3160d04e27c6fdd20778
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-12 13:46:15 -0800

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from pybaseball import statcast_batter, spraychart
+
+st.title("Pybaseball Spraychart")
+
+# Get the spraychart data for a specific player
+player_name = st.text_input('Enter player name:')
+data = statcast_batter('2021-01-01', '2021-12-31', 514888)
+s = spraychart(data, 'astros')
+fig = s.figure
+# Display the spraychart
+st.pyplot(fig)
